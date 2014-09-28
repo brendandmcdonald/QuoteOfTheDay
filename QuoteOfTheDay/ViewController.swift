7152be2acb6acc1d7cd705031047912e9b1eed79
Brendan McDonald <brendan@brendandmcdonald.com>
2014-09-27 22:34:49 -0700

    first commit

--- QuoteOfTheDay/ViewController.swift
+++ QuoteOfTheDay/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteText: UILabel!
+    @IBOutlet weak var quoteAuthor: UILabel!
+    @IBOutlet weak var quoteButton: UIButton!
+    
+    let quoteBook = QuoteBook()
+    let colors = Colors()
+    
+    func displayNextQuote(){
+        var nextQuoteIs = quoteBook.randomQuote()
+        
+        quoteText.text = nextQuoteIs.quote
+        quoteAuthor.text = nextQuoteIs.author
+        
+    }
+    
+    func updateBackgroundColorAndButtonTint(){
+        var randomColor = colors.randomColor()
+        
+        view.backgroundColor = randomColor
+        quoteButton.tintColor = randomColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        displayNextQuote()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +44,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func nextQuote() {
+        var randomColor = colors.randomColor()
+        
+        view.backgroundColor = randomColor
+        //quoteText.tintColor = randomColor
+        
+        var nextQuoteIs = quoteBook.randomQuote()
+        
+        quoteText.text = nextQuoteIs.quote
+        quoteAuthor.text = nextQuoteIs.author
+        
+        //quoteText.text = quoteBook.randomQuote().quote
+        //quoteAuthor.text = quoteBook.randomQuote().author
+        
+    }
 
 }
 
